4a5f132844632779750f33a1194ca176f38b0236
nachum freedman <nox@h-MacBook-Pro-sl-nachum.local>
2021-05-25 22:21:22 +0300

    tested and working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react"
 import './App.css';
 
 function App() {
+  const [list, setList] = useState([]);
+  const [currentValue, setCurrentValue] = useState("");
+
+  const deleteItem = (itemIndex) => {
+    let newList = list; 
+    newList[itemIndex] = undefined;
+    setList([...newList]);
+  };
+
+  const updateList = () => {
+    const newList = list; 
+    newList.push(currentValue);
+    setList(newList);
+    setCurrentValue("");
+    console.log(list)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <input 
+        placeholder="Shopping" 
+        value={currentValue}
+        onChange={(change)=>{
+          setCurrentValue(change.target.value);
+        }}></input>
+      <button onClick={updateList}>Add</button>
+      
+      
+      {list.map((item, index)=>{
+        if(item){
+          return (
+            <div key={Math.random()}>
+              <div>{item}</div>
+              <button onClick={()=>deleteItem(index)}>delete</button>
+            </div>
+            )
+        }
+      })}
+
     </div>
   );
 }
